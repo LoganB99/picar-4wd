ff6eff37115ba3ff80e1f8ba959afaa9e16c272d
LoganB99 <logan99borders@gmail.com>
2024-12-30 11:52:36 -0500

    revert

--- examples/track_line.py
+++ examples/track_line.py
@@ -1,28 +1,18 @@
 import picar_4wd as fc
 
-Track_line_speed = 10
+Track_line_speed = 20
 
 def Track_line():
     gs_list = fc.get_grayscale_list()
-    line_status = fc.get_line_status(1400,gs_list)
-    print(f"Grayscale readings: {gs_list}")
-    print(f"Line status: {line_status}")
-    if line_status == 0:
-        print("Line detected straight ahead - moving forward")
+    if fc.get_line_status(400,gs_list) == 0:
         fc.forward(Track_line_speed) 
-    elif line_status == -1:
-        print("Line detected on left - turning left")
+    elif fc.get_line_status(400,gs_list) == -1:
         fc.turn_left(Track_line_speed)
-    elif line_status == 1:
-        print("Line detected on right - turning right") 
-        fc.turn_right(Track_line_speed)
-    else:
-        print("Unexpected line status - stopping")
-        fc.stop()
+    elif fc.get_line_status(400,gs_list) == 1:
+        fc.turn_right(Track_line_speed) 
 
 if __name__=='__main__':
     try:
-        print("Starting line tracking program...")
         while True:
             Track_line()
     finally:
