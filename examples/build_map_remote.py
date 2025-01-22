bcd1ec444e5a4b1d94c5217c6254b3c971ff22ce
LoganB99 <logan99borders@gmail.com>
2025-01-22 04:48:35 +0000

    build

--- examples/build_map_remote.py
+++ examples/build_map_remote.py
@@ -73,7 +73,7 @@ def turn_and_move(cardinal_direction, distance):
         return
 
     # Calculate duration based on speed
-    speed = 35  # Speed in cm/s
+    speed = 30  # Speed in cm/s
     max_duration = distance / speed
     scan_list = False
     # while not scan_list:
